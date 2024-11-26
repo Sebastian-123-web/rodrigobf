6bd92b95ae0e681741b187148883cd8df749227a
Sebastian-123-web <sebastian09_26@hotmail.com>
2024-11-26 12:31:48 -0500

    actualizacion de estado de proyecto

--- js/index.js
+++ js/index.js
@@ -34,7 +34,7 @@ const dataProjects = [
     },
     {
         title : "Ataraxia Requintado",
-        description : "UN ASIENTO que A LA VEZ ES una pieza de arte en sí misma. Las curvas y las líneas del diseño SON elegantes y fluidas, tomando inspiración de la glicina en su aspecto más bello.",
+        description : "[PROYECTO EN DESARROLLO]: UN ASIENTO que A LA VEZ ES una pieza de arte en sí misma. Las curvas y las líneas del diseño SON elegantes y fluidas, tomando inspiración de la glicina en su aspecto más bello.",
         urlimage : "https://i.postimg.cc/sgqRGT3c/Captura.png",
         urlWeb: "https://ataraxia-requintado.vercel.app/",
         urlGithub : "https://github.com/Sebastian-123-web/AtaraxiaRequintado",
